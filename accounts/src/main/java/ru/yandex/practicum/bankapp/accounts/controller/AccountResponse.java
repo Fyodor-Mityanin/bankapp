662f598a8daa2281e1e65390efd834c2c8107096
Fedor MItyanin <79231292918@ya.ru>
2025-09-14 16:02:00 +0700

    fix response

--- accounts/src/main/java/ru/yandex/practicum/bankapp/accounts/controller/AccountResponse.java
+++ accounts/src/main/java/ru/yandex/practicum/bankapp/accounts/controller/AccountResponse.java
@@ -3,5 +3,9 @@ package ru.yandex.practicum.bankapp.accounts.controller;
 import java.util.List;
 
 public record AccountResponse(Long id, String login, List<BalanceDto> accounts) {
-    public record BalanceDto(Long id, String currency, Double amount) {}
+    public record BalanceDto(boolean exists, CurrencyDto currency, Double value) {
+    }
+
+    public record CurrencyDto(String name, String title) {
+    }
 }
--- accounts/src/main/java/ru/yandex/practicum/bankapp/accounts/service/AccountMapper.java
+++ accounts/src/main/java/ru/yandex/practicum/bankapp/accounts/service/AccountMapper.java
@@ -9,13 +9,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.yandex.practicum.bankapp.accounts.controller.AccountRequest;
 import ru.yandex.practicum.bankapp.accounts.controller.AccountResponse;
 import ru.yandex.practicum.bankapp.accounts.entity.Account;
+import ru.yandex.practicum.bankapp.accounts.entity.AccountBalance;
+
+import java.util.Map;
 
 @Mapper
 public abstract class AccountMapper {
 
+    private static final Map<String, String> currencyMap =
+            Map.of("RUB", "Рубль", "USD", "Доллар", "CNY", "Юань");
+
     @Setter(onMethod_ = @Autowired)
     protected PasswordEncoder passwordEncoder;
 
+    @Mapping(target = "accounts", source = "balances")
     abstract AccountResponse toResponse(Account source);
 
     @Mapping(target = "balances", ignore = true)
@@ -27,5 +34,10 @@ public abstract class AccountMapper {
     @Named("encodePassword")
     public String encodePassword(String password) {
         return passwordEncoder.encode(password);
+    }
+
+    protected AccountResponse.BalanceDto toResponse(AccountBalance source) {
+        AccountResponse.CurrencyDto currencyDto = new AccountResponse.CurrencyDto(source.getCurrency(), currencyMap.get(source.getCurrency()));
+        return  new AccountResponse.BalanceDto(true, currencyDto, source.getAmount());
     }
 }
